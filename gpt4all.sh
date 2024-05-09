aca3d3fdc65bb735c12b265e824984c227350ec9
joshMGen <157756776+joshMGen@users.noreply.github.com>
2024-05-09 14:41:45 -0600

    Add files via upload

--- gpt4all.sh
+++ gpt4all.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-sudo git clone https://github.com/joshMGen/gpt4all_reference /home/ubuntu/gpt4all
+# sudo git clone https://github.com/joshMGen/gpt4all_reference /home/ubuntu/gpt4all
 
-sudo chmod -R 777 /home/ubuntu/gpt4all
+# sudo chmod -R 777 /home/ubuntu/gpt4all
 
-sudo pip install typing-extensions
-sudo pip install typer
-sudo pip install gpt4all
+# sudo pip install typing-extensions
+# sudo pip install typer
+# sudo pip install gpt4all
 
 python3 app.py repl --model gpt4all-falcon-newbpe-q4_0.gguf
 
